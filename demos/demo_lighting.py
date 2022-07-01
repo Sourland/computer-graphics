04b5e3ac14bcf4d3f94683f35b7ebeebe4ff87e2
sourland <jem2010@windowslive.com>
2022-07-01 19:26:11 +0300

    fix picture saving

--- demos/demo_lighting.py
+++ demos/demo_lighting.py
@@ -27,50 +27,52 @@ focal = 70
 img = render_object('Ambient', 'Gouraud', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts,
                     vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Ambient.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Ambient.jpeg')
 plt.show()
 
 img = render_object('Diffuse', 'Gouraud', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts,
                     vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Diffuse.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Diffuse.jpeg')
 plt.show()
 
 img = render_object('Specular', 'Gouraud', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts,
                     vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Specular.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_Specular.jpeg')
 plt.show()
 
 img = render_object('All', 'Gouraud', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts, vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_All.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Gouraud_All.jpeg')
 plt.show()
 
 img = render_object('Ambient', 'Phong', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts, vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Phong_Ambient.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Phong_Ambient.jpeg')
 plt.show()
 
 img = render_object('Diffuse', 'Phong', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts, vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Phong_Diffuse.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Phong_Diffuse.jpeg')
 plt.show()
 
 img = render_object('Specular', 'Phong', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts, vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Phong_Specular.jpeg')
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Phong_Specular.jpeg')
 plt.show()
 
 img = render_object('All', 'Phong', focal, cam_eye, cam_lookat, cam_up, bg_color, M, N, H, W, verts, vertex_colors,
                     face_indices, ka, kd, ks, n, light_positions, light_intensities, Ia)
-plt.savefig('../results/Assignment 3 - View/Dog_Phong_All.jpeg')
+
 plt.imshow(img)
+plt.savefig('../results/Assignment 3 - View/Dog_Phong_All.jpeg')
 plt.show()
+
